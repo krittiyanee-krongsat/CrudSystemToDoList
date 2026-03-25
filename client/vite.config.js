c1c9006b8acc7c560892fe174fbce6999e604cdd
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-03-25 17:38:35 +0700

    import tailwind css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
